b8c24847fef727acf165fa8cc9fd3e42de5e25a6
ishanu <ishanu.chattopadhyay@gmail.com>
2020-01-06 10:03:11 -0600

    upd

--- midterm/code_snippets/read_unequal_lines.py
+++
@@ -1,5 +0,0 @@
-fname='files_with_unequal_no_of_columns.dat'
-with open(fname) as f:
-    content = f.readlines()
-content = [x.strip() for x in content] 
-# you may also want to remove whitespace characters like `\n` at the end of each line
