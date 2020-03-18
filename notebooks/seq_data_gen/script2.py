1b532ce716fa6d470902a5c66cb51f38b7e5a920
ishanu chattopadhyay <ishanu.chattopadhyay@gmail.com>
2020-03-18 07:23:21 -0500

    upd

--- notebooks/seq_data_gen/script2.py
+++ notebooks/seq_data_gen/script2.py
@@ -2,7 +2,7 @@ import seqpull as sq
 import pandas as pd
 import pylab as plt
 import numpy as np
-import pickle
+import cPickle
 
 #recordfile='SARS-CoV-2.rec'
 #pkl_file=open(recordfile,'rb')
@@ -12,13 +12,24 @@ import pickle
 #                         N=5000,LMAX=35000,
 #                         integer_dataframe=True,heatmap=True)
 
-recordfile='2019-nCoVrec'
+recordfile='2019-nCoV.rec'
 pkl_file=open(recordfile,'rb')
-records_covid=pickle.load(pkl_file)
+records_covid=cPickle.load(pkl_file)
 [SFnc,SFinc]=sq.procSequence(records_covid,
                              begIndex=21563,endIndex=25384,
                              N=6000,LMAX=35000,
                              integer_dataframe=True,heatmap=False)
 SFnc.to_csv('covid19.csv',index=None)
+
+
+recordfile='betacoronav.rec'
+pkl_file=open(recordfile,'rb')
+records_covid=cPickle.load(pkl_file)
+[SFnc,SFinc]=sq.procSequence(records_covid,
+                             begIndex=21563,endIndex=25384,
+                             N=6000,LMAX=35000,
+                             integer_dataframe=True,heatmap=False)
+SFnc.sample(1000).to_csv('betacorona.csv',index=None)
+
 
 #accession_ncov=list(set(list(SFnc.accession.values)).intersection(set(list(SF.accession.values))))
